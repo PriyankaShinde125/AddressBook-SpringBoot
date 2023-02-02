46295eb2cb05a708cd6509406541e503c628f39f
PriyankaShinde125 <pkshinde125@gmail.com>
2023-02-02 12:47:22 +0530

    INITIALIZED : Address book with springboot

---
+++ src/main/java/com/example/addressbooksystem/AddressbooksystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.addressbooksystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressbooksystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressbooksystemApplication.class, args);
+	}
+
+}
